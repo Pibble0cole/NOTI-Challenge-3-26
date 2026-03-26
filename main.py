95aecc4dfcdb02e2cea2fac871cce6c6deceeb9b
Pibble0cole <8846256@philasd.org>
2026-03-26 10:42:41 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ sale_items = {"hammer": 10.45, "screwdriver set": 52.50, "saw": 15.75, "level": 
 total = 0
 ordered_items = []
 
+print("sale list")
+for key, value in sale_items.items():
+    print(key + ": $" + str(value))
+print()
+
 #in a while loop ask the user for their order items then write done when finished
 #inside loop i need to add to the total and place the items ordered in the list
 while True:
-    print("sale list")
-    for key, value in sale_items.items():
-        print(key + ": $" + str(value))
     order = input("(type done to exit)what do you want to order?: ")
     if order == "done":
         break
@@ -25,10 +27,11 @@ while True:
         print("invalid input")
         continue
     print()
+
     ordered_items.append((order, quantity))
     total += sale_items[order] * quantity
     print("item added")
 
-#ourside of loop i will print the items and totals for the customer
+#outside of loop i will print the items and totals for the customer
 print(ordered_items)
 print("Total: " + str(total))
